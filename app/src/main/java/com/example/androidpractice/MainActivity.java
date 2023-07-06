8fc0708744a38a1cb445b053baa036a26958f042
Luckykandpal021 <luckykandpal059@gmail.com>
2023-07-06 22:55:58 +0530

    BMI Calculator

--- app/src/main/java/com/example/androidpractice/MainActivity.java
+++ app/src/main/java/com/example/androidpractice/MainActivity.java
@@ -1,22 +1,43 @@
 package com.example.androidpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText editWeight = findViewById(R.id.editWeight);
+        EditText editHeightFt = findViewById(R.id.editHeightFt);
+        EditText editHeightIn = findViewById(R.id.editHeightIn);
+        Button bmiCalculate = findViewById(R.id.bmiCalculate);
+        TextView bmiResult = findViewById(R.id.bmiResult);
 
-        // assigning ID of the toolbar to a variable
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        bmiCalculate.setOnClickListener(v -> {
+            int inputWeight = Integer.parseInt(editWeight.getText().toString());
+            int inputHeightFt = Integer.parseInt(editHeightFt.getText().toString());
+            int inputHeightIn = Integer.parseInt(editHeightIn.getText().toString());
+            int totalIn = inputHeightFt * 12 + inputHeightIn;
+            double totalCm = totalIn * 2.54;
+            double totalM = totalCm / 100;
+            double bmi = inputWeight / (totalM * totalM);
+            String bmiResultText;
+            if (bmi > 25) {
+                bmiResultText = "You Are Overweight";
+            } else if (bmi < 18.5) {
+                bmiResultText = "You Are Underweight";
+            } else {
+                bmiResultText = "You Are Healthy";
+            }
 
-        // using toolbar as ActionBar
-        setSupportActionBar(toolbar);
-
-
+            bmiResult.setText(bmiResultText);
+        });
     }
 }
