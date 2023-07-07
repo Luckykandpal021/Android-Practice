e4831fa7d45712504335e441e4c1c53e707946b0
Luckykandpal021 <luckykandpal059@gmail.com>
2023-07-07 11:29:10 +0530

    New Screen Created named SecondActivity

--- app/src/main/java/com/example/androidpractice/SecondActivity.java
+++ app/src/main/java/com/example/androidpractice/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.example.androidpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 //import android.util.Log;
 import android.content.Intent;
 import android.widget.Button;
@@ -13,11 +14,19 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Button nextScreen = findViewById(R.id.next);
+
+        Toolbar toolbar = findViewById(R.id.secondAppBar); //for using by default features of tool bar by androidX
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        toolbar.setTitle("My Tool Bar");
+//        toolbar.sutSubtitle()
 //        Log.d("MyApp", "This is a debug message.");
 //        Log.e("MyApp", "An error occurred!");
-        nextScreen.setOnClickListener(V-> {
-                Intent iNext = new Intent(SecondActivity.this, MainActivity.class);
-                startActivity(iNext);
+        nextScreen.setOnClickListener(V -> {
+            Intent iNext = new Intent(SecondActivity.this, MainActivity.class);
+            startActivity(iNext);
 
         });
     }
